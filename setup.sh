641166061034f7cb8402248db68cb90797e756c6
kirscht <kirscht@gmail.com>
2020-06-03 13:13:45 -0700

    Add ansible installation

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ set -x
 
 export LABDIR="${HOME}/my_az_deployment/"
 export REMOTEHOST="http://localhost:8080/lab12/"
-export REMOTEHOST=" -fsSL -H 'Cache-Control: no-cache' https://lab12.raw.kirscht.com/"
+export REMOTEHOST=" -fsSL https://lab12.raw.kirscht.com/"
 
 mkdir -p ${LABDIR}/{.config,bin,RedTeam}
 
