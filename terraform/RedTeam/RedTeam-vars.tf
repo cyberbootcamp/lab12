1ec7a044dc550af1305bf5d8d1fb3871146ce85d
kirscht <kirscht@gmail.com>
2020-06-02 23:27:48 -0700

    Add ansible installation

--- terraform/RedTeam/RedTeam-vars.tf
+++ terraform/RedTeam/RedTeam-vars.tf
@@ -6,13 +6,13 @@ variable "machine1_name" {}
 variable "machine1_osdisk_type" {}
 variable "machine1_adminusername" {}
 variable "machine1_publickey" {}
-variable "machine1_custom_data" {}
+#variable "machine1_custom_data" {}
 
 variable "machine2_size" {}
 variable "machine2_name" {}
 variable "machine2_osdisk_type" {}
 variable "machine2_adminusername" {}
 variable "machine2_publickey" {}
-variable "machine2_custom_data" {}
+#variable "machine2_custom_data" {}
 
 variable "my_ip_address" {}
