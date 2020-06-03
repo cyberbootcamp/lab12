00ee8e90f2fdf44f89f02b98c4bc3ad8c025c463
kirscht <kirscht@gmail.com>
2020-06-02 22:46:18 -0700

    Add ansible installation

--- setup.sh
+++ setup.sh
@@ -5,6 +5,8 @@ export REMOTEHOST=" -H 'Cache-Control: no-cache' https://raw.githubusercontent.c
 
 mkdir -p ${LABDIR}/{.config,bin,RedTeam}
 
+[[ -e ${LABDIR}/site.yml ]] && rm ${LABDIR}/site.yml
+
 curl -o ${LABDIR}/site.yml https://raw.githubusercontent.com/cyberbootcamp/lab12/master/site.yml
 
 which ansible || sudo apt-get install -y ansible
