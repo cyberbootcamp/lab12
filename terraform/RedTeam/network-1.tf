63558791459df2eebdd854d784ff0df6f9e07eb9
kirscht <kirscht@gmail.com>
2020-06-03 19:03:02 -0700

    Add ansible installation

--- terraform/RedTeam/network-1.tf
+++ terraform/RedTeam/network-1.tf
@@ -1,8 +1,8 @@
-resource "azurerm_network_ddos_protection_plan" "example" {
-  name                = "ddospplan1"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
+#resource "azurerm_network_ddos_protection_plan" "example" {
+#  name                = "ddospplan1"
+#  location            = azurerm_resource_group.rg.location
+#  resource_group_name = azurerm_resource_group.rg.name
+#}
 
 resource "azurerm_network_security_group" "example" {
   name                = "acceptanceTestSecurityGroup1"
@@ -18,10 +18,10 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["10.0.0.0/16"]
   # dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  ddos_protection_plan {
-    id     = azurerm_network_ddos_protection_plan.example.id
-    enable = true
-  }
+#  ddos_protection_plan {
+#    id     = azurerm_network_ddos_protection_plan.example.id
+#    enable = true
+#  }
 
   subnet {
     name           = "accounting"
