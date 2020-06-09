ebd481e6eb0a6e28fb0b4251ea6c24d2152066c3
kirscht <kirscht@gmail.com>
2020-06-08 19:18:27 -0700

    Updating config

--- terraform/RedTeam/vm-1.tf
+++ terraform/RedTeam/vm-1.tf
@@ -1,16 +1,4 @@
 
-
-resource "azurerm_public_ip" "redteamip" {
-  name                = "acceptanceTestPublicIp1"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  allocation_method   = "Static"
-
-  tags = {
-    environment = "Production"
-  }
-}
-
 # Create network interface
 resource "azurerm_network_interface" "redteam-vm1-nic" {
   name                      = "vm1NIC"
