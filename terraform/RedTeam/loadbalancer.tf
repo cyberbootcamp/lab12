7d00c9ebfd7387ce4129c119aa26c44a1e052408
kirscht <kirscht@gmail.com>
2020-06-08 21:17:19 -0700

    Updating config

--- terraform/RedTeam/loadbalancer.tf
+++ terraform/RedTeam/loadbalancer.tf
@@ -1,9 +1,9 @@
-#resource "azurerm_public_ip" "rt" {
-#  name                = "PublicIPForLB"
-#  location            = azurerm_resource_group.rg.location
-#  resource_group_name = azurerm_resource_group.rg.name
-#  allocation_method   = "Static"
-#}
+resource "azurerm_public_ip" "rt" {
+  name                = "PublicIPForLB"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+}
 
 resource "azurerm_lb" "rt" {
   name                = "RedTeamLoadBalancer"
@@ -12,7 +12,7 @@ resource "azurerm_lb" "rt" {
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.redteamip.id
+    public_ip_address_id = azurerm_public_ip.rt.id
   }
 }
 
