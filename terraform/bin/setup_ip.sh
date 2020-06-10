58d7149024bc2660ff40c02ccd1985c2aef00c67
kirscht <kirscht@gmail.com>
2020-06-09 22:28:46 -0700

    Updating config

--- terraform/bin/setup_ip.sh
+++ terraform/bin/setup_ip.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
 {
-	echo -n 'enter your ip address from https://whatismyipaddress.com/'
-	read
-	echo export TF_VAR_my_ip_address=${REPLY} | tee .config/TF_VAR_my_ip_address
+	#echo -n 'enter your ip address from https://whatismyipaddress.com/'
+	#read
+	export myip=$(curl 'https://api.ipify.org?format=json' | jq -r '.ip')
+	echo export TF_VAR_my_ip_address=${myip} | tee .config/TF_VAR_my_ip_address
 }
 chmod +x .config/TF_VAR_my_ip_address
