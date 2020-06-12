96093cc7950ae20e3834987b9836345f493fc62c
kirscht <kirscht@gmail.com>
2020-06-11 21:03:04 -0700

    Version 2

--- terraform/RedTeam/network-1.tf
+++ terraform/RedTeam/network-1.tf
@@ -1,45 +1,45 @@
-resource "azurerm_network_ddos_protection_plan" "example" {
-  name                = "ddospplan1"
+resource "azurerm_network_ddos_protection_plan" "redteam" {
+  name                = format("%sddospplan1", var.resource_group_name)
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
-resource "azurerm_network_security_group" "example" {
-  name                = "acceptanceTestSecurityGroup1"
+resource "azurerm_network_security_group" "redteam" {
+  name                = format("%sSecurityGroup1", var.resource_group_name)
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "virtualNetworkRedTeam1"
+resource "azurerm_virtual_network" "redteam" {
+  name                = format("%svirtualNetwork1"var.resource_group_name)
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.0.0.0/16"]
   # dns_servers         = ["10.0.0.4", "10.0.0.5"]
-  depends_on = [azurerm_network_security_group.example]
+  depends_on = [azurerm_network_security_group.redteam]
 
   ddos_protection_plan {
-    id     = azurerm_network_ddos_protection_plan.example.id
+    id     = azurerm_network_ddos_protection_plan.redteam.id
     enable = false
   }
 
-#  subnet {
-#    name           = "accounting"
-#    address_prefix = "10.0.1.0/24"
-#  }
+  subnet {
+    name           = "dmz"
+    address_prefix = "10.0.1.0/24"
+  }
 
-#  subnet {
-#    name           = "human_resources"
-#    address_prefix = "10.0.2.0/24"
-#    security_group = azurerm_network_security_group.example.id
-#  }
+  subnet {
+    name           = "webtier"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
 
-#  subnet {
-#    name           = "finance"
-#    address_prefix = "10.0.3.0/24"
-#    security_group = azurerm_network_security_group.example.id
-#  }
+  subnet {
+    name           = "apptier"
+    address_prefix = "10.0.3.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
 
   tags = {
     environment = "Production"
